069dda3432bc65f27665f03cad752529d55d03ce
Abdul Kader <88508957+iabdulkader@users.noreply.github.com>
2023-03-14 15:36:23 +0000

    Added resume to download

--- components/header/Ribbon.js
+++ components/header/Ribbon.js
@@ -3,7 +3,7 @@ import styles from "./Header.module.css";
 export default function Ribbon() {
   return (
     <div className={styles.ribbon}>
-      <a href="" className="btn">Download CV</a>
+      <a href="/abdulkader.pdf" className="btn">Download CV</a>
       <a href="#contact" className="btn btn-primary">Let&apos;s Talk</a>
     </div>)
 }
