19308643ab774a159547fd132b149477a67dcce7
Abdul Kader <abdulkadernow@gmail.com>
2023-04-07 00:27:50 +0600

    Minor

--- src/components/Hero/HeroText.tsx
+++ src/components/Hero/HeroText.tsx
@@ -5,7 +5,9 @@ type Props = {};
 function HeroText({}: Props) {
   return (
     <div className="w-full flex justify-center h-96 items-center relative">
-      <h1 className="font-[Satoshi] text-[5rem] font-bold">abdul kader</h1>
+      <h1 className="font-[Satoshi] text-[3rem] lg:text-[5rem] font-bold">
+        abdul kader
+      </h1>
     </div>
   );
 }
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -10,19 +10,20 @@ import Logo from "../../assets/logo.svg";
 import Link from "next/link";
 import HeroText from "./HeroText";
 import { useParallax } from "react-scroll-parallax";
+import HeroBoxes from "./HeroBoxes";
 
 type Props = {};
 
 function Hero({}: Props) {
   const target = useRef<HTMLDivElement>(null);
   const train = useParallax({
-    speed: 5,
-    scale: [1, 0.2],
+    speed: 0.5,
+    scale: [1, 0.1],
     targetElement: target.current as HTMLDivElement,
   });
 
   const cloud = useParallax({
-    speed: 100,
+    speed: 60,
     targetElement: target.current as HTMLDivElement,
   });
 
@@ -38,54 +39,7 @@ function Hero({}: Props) {
         ref={cloud.ref as React.RefObject<HTMLDivElement>}
         className="h-full grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-5"
       >
-        <div className="h-[24rem] lg:h-[30rem] px-2 lg:pl-5 lg:px-0">
-          <a href="#about">
-            <Box
-              className="bg-[#2800E7]"
-              animatedText="As an experienced and passionate full stack developer, I bring a diverse range of technical expertise to the table. From building responsive user interfaces with React, Next.js, and Svelte to designing scalable backend systems with Express, Node, Python, Django, and Docker, I am comfortable with a range of different technologies. With excellent communication skills and a collaborative mindset, I thrive in team-oriented environments and always aim to add value to any project I am a part of."
-              showWithOutHover
-              duration={500}
-              topRight={
-                <div className="w-12 h-12">
-                  <Image src={Logo} alt="logo" />
-                </div>
-              }
-              bottomLeft={<div>About</div>}
-              bottomRight={<AiOutlineDown />}
-            />
-          </a>
-        </div>
-
-        <div className="h-[24rem] lg:h-[30rem] grid grid-rows-2 px-2 lg:pr-5 lg:px-0 gap-3 lg:gap-5 ">
-          <div className="h-full gap-3 lg:gap-5 grid grid-cols-2">
-            <Link href="#skills">
-              <Box
-                duration={100}
-                className="bg-[#FFFFFF] text-slate-900"
-                animatedText="Joshare. jobin. modal-rt. link-shortner. joshare-file-service-microservice. 1337x API."
-                bottomLeft={<div>Works</div>}
-                bottomRight={<BsTerminalPlus />}
-              />
-            </Link>
-            <a href="#skills">
-              <Box
-                duration={350}
-                className="bg-[#344e41] "
-                animatedText="React, Nextjs, Svelte, Redux, Context-API, Tailwind, Chakra, Material-UI, Express, Socket-IO, Node, Python, Django, Docker, MongoDB, PostgreSQL, MySQL, Redis, Heroku, Netlify, Vercel, DigitalOcean, Firebase, Git, Github, Trello, Slack, Discord, Zoom, Figma, Adobe Photoshop, Adobe Illustrator."
-                bottomLeft={<div>Skills</div>}
-                bottomRight={<BiNetworkChart />}
-              />
-            </a>
-          </div>
-
-          <Box
-            duration={80}
-            className="bg-[#FEF48C] text-slate-900"
-            animatedText="Contact me for any work or suggestions that you would like to give."
-            bottomLeft={<div>Contact</div>}
-            bottomRight={<BiSend />}
-          />
-        </div>
+        <HeroBoxes />
       </div>
     </div>
   );
