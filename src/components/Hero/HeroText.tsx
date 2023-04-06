277c3bf32397c3c627fe026d9575692d9cf3b513
Abdul Kader <abdulkadernow@gmail.com>
2023-04-07 00:58:22 +0600

    Test

--- src/components/Hero/HeroText.tsx
+++ src/components/Hero/HeroText.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  percentage: number;
+};
 
-function HeroText({}: Props) {
+function HeroText({ percentage }: Props) {
   return (
     <div className="w-full flex justify-center h-96 items-center relative">
-      <h1 className="font-[Satoshi] text-[3rem] lg:text-[5rem] font-bold">
+      <h1
+        className={` font-[Satoshi] text-[3rem] lg:text-[5rem] font-bold`}
+        style={{
+          scale: `${percentage === 0 ? 0 : (percentage + 45) / 100}`,
+          opacity: `${percentage === 0 ? 0 : (percentage + 45) / 100}`,
+        }}
+      >
         abdul kader
       </h1>
     </div>
--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Box from "../Box";
 import Image from "next/image";
 import { AiOutlineDown } from "react-icons/ai";
@@ -16,29 +16,63 @@ type Props = {};
 
 function Hero({}: Props) {
   const target = useRef<HTMLDivElement>(null);
-  const train = useParallax({
-    speed: 0.5,
-    scale: [1, 0.1],
-    targetElement: target.current as HTMLDivElement,
-  });
+  const [percentage, setPercentage] = React.useState(55);
+  // const train = useParallax({
+  //   speed: 0.5,
+  //   scale: [1, 0.1],
+  //   targetElement: target.current as HTMLDivElement,
+  // });
 
-  const cloud = useParallax({
-    speed: 60,
-    targetElement: target.current as HTMLDivElement,
-  });
+  // const cloud = useParallax({
+  //   speed: 60,
+  //   targetElement: target.current as HTMLDivElement,
+
+  // });
+
+  const divRef = useRef(null);
+
+  const handleScroll = () => {
+    const scrollTop = target.current!.scrollTop;
+    console.log("Scroll Top:", target.current);
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const divElement = target.current; // Get the div element from the ref
+      if (divElement) {
+        const rect = divElement.getBoundingClientRect(); // Get the bounding rect of the div element
+        const windowHeight =
+          window.innerHeight || document.documentElement.clientHeight; // Get the height of the window
+        const percentage = Math.max(
+          0,
+          Math.min(
+            100,
+            ((rect.bottom > 0 ? rect.bottom : 0) / windowHeight) * 100
+          )
+        );
+        setPercentage(Math.floor(percentage));
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [target]);
 
   return (
-    <div ref={target} className="relative min-h-screen h-full w-full">
-      <div className=" " ref={train.ref as React.RefObject<HTMLDivElement>}>
-        <HeroText />
+    <div
+      onScroll={handleScroll}
+      className="relative min-h-screen h-full w-full"
+    >
+      <div ref={target} className=" ">
+        <HeroText percentage={percentage} />
       </div>
 
       {/* Boxes */}
 
-      <div
-        ref={cloud.ref as React.RefObject<HTMLDivElement>}
-        className="h-full grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-5"
-      >
+      <div className="h-full grid grid-cols-1 lg:grid-cols-2 gap-3 lg:gap-5">
         <HeroBoxes />
       </div>
     </div>
